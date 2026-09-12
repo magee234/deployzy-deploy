4601cf1dd8eb1381b4fab2b17d24f4a5358c6e8a
magee234 <61399599+magee234@users.noreply.github.com>
2026-09-12 22:12:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const UUID = process.env.UUID || '84705c0d-5036-44b1-a07e-d1582e653595'; // 使�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
-const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'deployzy.11122234.xyz';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiOWQzZTVmOTUwOWQ4YTJhMTBiNzA1OTBkMWVmNGNmYWIiLCJ0IjoiNmM4YjU0MDItZjNlYi00OTFlLWI1NjEtZGJkNjYzYTc3NzFlIiwicyI6Ik5XWTNZVFl4T0dFdE5tSmhOaTAwWTJRekxXSmtNak10WldVeFpEazRPRFpsTnpJeCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_PORT = process.env.ARGO_PORT || 2777;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || 'Galaxy';                  // 节点名称
+const NAME = process.env.NAME || 'deployzy';                  // 节点名称
 
 // 创建运行文件夹
 if (!fs.existsSync(FILE_PATH)) {
